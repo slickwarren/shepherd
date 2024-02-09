ee75165fbae5db292757bed76e0941d5f779d14f
Caleb Warren <slickwarren21@gmail.com>
2024-02-09 10:36:37 -0800

    updating agentCustomization to v1 objects

--- extensions/clusters/clusterconfig.go
+++ extensions/clusters/clusterconfig.go
@@ -1,36 +1,37 @@
 package clusters
 
 import (
+	v1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
 	rkev1 "github.com/rancher/rancher/pkg/apis/rke.cattle.io/v1"
 	management "github.com/rancher/shepherd/clients/rancher/generated/management/v3"
 	provisioningInput "github.com/rancher/shepherd/extensions/provisioninginput"
 )
 
 type ClusterConfig struct {
-	KubernetesVersion    string                                   `json:"kubernetesVersion" yaml:"kubernetesVersion"`
-	CNI                  string                                   `json:"cni" yaml:"cni"`
-	PSACT                string                                   `json:"psact" yaml:"psact"`
-	PNI                  bool                                     `json:"pni" yaml:"pni"`
-	NodePools            []provisioningInput.NodePools            `json:"nodepools" yaml:"nodepools"`
-	MachinePools         []provisioningInput.MachinePools         `json:"machinepools" yaml:"machinepools"`
-	CloudProvider        string                                   `json:"cloudProvider" yaml:"cloudProvider"`
-	Providers            *[]string                                `json:"providers" yaml:"providers"`
-	NodeProviders        *[]string                                `json:"nodeProviders" yaml:"nodeProviders"`
-	Hardened             bool                                     `json:"hardened" yaml:"hardened"`
-	AddOnConfig          *provisioningInput.AddOnConfig           `json:"addonConfig" yaml:"addonConfig"`
-	AgentEnvVars         *[]rkev1.EnvVar                          `json:"agentEnvVars" yaml:"agentEnvVars"`
-	AgentEnvVarsRKE1     *[]management.EnvVar                     `json:"agentEnvVarsRKE1" yaml:"agentEnvVarsRKE1"`
-	ClusterAgent         *management.AgentDeploymentCustomization `json:"clusterAgent" yaml:"clusterAgent"`
-	FleetAgent           *management.AgentDeploymentCustomization `json:"fleetAgent" yaml:"fleetAgent"`
-	ETCD                 *rkev1.ETCD                              `json:"etcd" yaml:"etcd"`
-	ETCDRKE1             *management.ETCDService                  `json:"etcdRKE1" yaml:"etcdRKE1"`
-	LabelsAndAnnotations *provisioningInput.LabelsAndAnnotations  `json:"labelsAndAnnotations" yaml:"labelsAndAnnotations"`
-	Networking           *provisioningInput.Networking            `json:"networking" yaml:"networking"`
-	Registries           *provisioningInput.Registries            `json:"registries" yaml:"registries"`
-	UpgradeStrategy      *rkev1.ClusterUpgradeStrategy            `json:"upgradeStrategy" yaml:"upgradeStrategy"`
-	Advanced             *provisioningInput.Advanced              `json:"advanced" yaml:"advanced"`
-	ClusterSSHTests      []provisioningInput.SSHTestCase          `json:"clusterSSHTests" yaml:"clusterSSHTests"`
-	CRIDockerd           bool                                     `json:"criDockerd" yaml:"criDockerd"`
+	KubernetesVersion    string                                  `json:"kubernetesVersion" yaml:"kubernetesVersion"`
+	CNI                  string                                  `json:"cni" yaml:"cni"`
+	PSACT                string                                  `json:"psact" yaml:"psact"`
+	PNI                  bool                                    `json:"pni" yaml:"pni"`
+	NodePools            []provisioningInput.NodePools           `json:"nodepools" yaml:"nodepools"`
+	MachinePools         []provisioningInput.MachinePools        `json:"machinepools" yaml:"machinepools"`
+	CloudProvider        string                                  `json:"cloudProvider" yaml:"cloudProvider"`
+	Providers            *[]string                               `json:"providers" yaml:"providers"`
+	NodeProviders        *[]string                               `json:"nodeProviders" yaml:"nodeProviders"`
+	Hardened             bool                                    `json:"hardened" yaml:"hardened"`
+	AddOnConfig          *provisioningInput.AddOnConfig          `json:"addonConfig" yaml:"addonConfig"`
+	AgentEnvVars         *[]rkev1.EnvVar                         `json:"agentEnvVars" yaml:"agentEnvVars"`
+	AgentEnvVarsRKE1     *[]management.EnvVar                    `json:"agentEnvVarsRKE1" yaml:"agentEnvVarsRKE1"`
+	ClusterAgent         *v1.AgentDeploymentCustomization        `json:"clusterAgent" yaml:"clusterAgent"`
+	FleetAgent           *v1.AgentDeploymentCustomization        `json:"fleetAgent" yaml:"fleetAgent"`
+	ETCD                 *rkev1.ETCD                             `json:"etcd" yaml:"etcd"`
+	ETCDRKE1             *management.ETCDService                 `json:"etcdRKE1" yaml:"etcdRKE1"`
+	LabelsAndAnnotations *provisioningInput.LabelsAndAnnotations `json:"labelsAndAnnotations" yaml:"labelsAndAnnotations"`
+	Networking           *provisioningInput.Networking           `json:"networking" yaml:"networking"`
+	Registries           *provisioningInput.Registries           `json:"registries" yaml:"registries"`
+	UpgradeStrategy      *rkev1.ClusterUpgradeStrategy           `json:"upgradeStrategy" yaml:"upgradeStrategy"`
+	Advanced             *provisioningInput.Advanced             `json:"advanced" yaml:"advanced"`
+	ClusterSSHTests      []provisioningInput.SSHTestCase         `json:"clusterSSHTests" yaml:"clusterSSHTests"`
+	CRIDockerd           bool                                    `json:"criDockerd" yaml:"criDockerd"`
 }
 
 // ConvertConfigToClusterConfig converts the config from (user) provisioning input to a cluster config
